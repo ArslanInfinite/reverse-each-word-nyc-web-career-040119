3b3c282be7c9bd2358022ec7f9bca33935d0fc15
Arslan Ali <ali.arslan319@gmail.com>
2019-03-27 20:25:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence1)
   new_sentence = []
   new_sentence = sentence1.split(" ")
+  brand_new_sentence = []
   new_sentence.each do |word|
-    brand_new_sentence = []
       brand_new_sentence << word
     end
 new_sentence.join("").reverse
